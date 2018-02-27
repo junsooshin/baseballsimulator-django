6841e42b1e64df62ea9d2c394a54c92bbb007ad1
Jun Soo Shin <jsshin41@gmail.com>
2018-02-26 23:04:27 -0500

    Populated the database by using pg:push. Restore the code back to normal dev settings

--- baseballproject/settings/base.py
+++ baseballproject/settings/base.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['baseballsimulator.herokuapp.com', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['baseballsimulator.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
