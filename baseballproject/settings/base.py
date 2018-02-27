a9c267af14b20b3d619488fae2858afa52a1b72c
Jun Soo Shin <jsshin41@gmail.com>
2018-02-26 21:19:34 -0500

    Trying to fix bugs 2

--- baseballproject/settings/base.py
+++ baseballproject/settings/base.py
@@ -13,7 +13,8 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['baseballsimulator.herokuapp.com', '127.0.0.1']
+# ALLOWED_HOSTS = ['baseballsimulator.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
