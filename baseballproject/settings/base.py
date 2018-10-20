9eede66550cadbae02bb067590d18bc8bd1e3cfd
Jun Soo Shin <JunSooShin@MacBook-Pro.fios-router.home>
2018-10-19 20:42:08 -0400

    added localhost to allowed host

--- baseballproject/settings/base.py
+++ baseballproject/settings/base.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['baseballsimulator.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['baseballsimulator.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
